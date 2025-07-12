2fb126ef0093ec25275aa49b547169584e5601e6
Richard Quansah <rich.quansah03@gmail.com>
2025-07-12 13:24:33 +0000

    Add script that prints square

---
+++ 8-square.js
@@ -0,0 +1,14 @@
+const sqr = process.argv[2];
+const size = parseInt(sqr);
+
+if (isNaN(size)) {
+    console.log('Missing size');
+} else {
+        for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+    row += 'X';
+    }
+    console.log(row);
+    }
+}
